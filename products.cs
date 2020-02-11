7e2c749bc83bf60bc56d8090ac6e522a0166f438
Andre Gomez <andremgm1@hotmail.com>
2020-02-11 16:29:08 -0600

    adding all code samples

---
+++ products.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace firstDuplicate
+{
+    public class Matrix
+    {
+        string name { get; set; }
+        public string JID { get; set; }
+        string tier { get; set; }
+        string PM { get; set; }
+        string backupPm { get; set; }
+        string ceLead { get; set; }
+        string podLead { get; set; }
+        string solutionLead { get; set; }
+        string POD { get; set; }
+        string PODSubCategory { get; set; }
+        string liveDate { get; set; }
+        string L3 { get; set; }
+        string customerTheather { get; set; }
+        string region { get; set; }
+        string country { get; set; }
+        string solutionSuite { get; set; }
+        string agreetmentType { get; set; }
+        string model { get; set; }
+        string phase { get; set; }
+        string products { get; set; }
+        string productionDc { get; set; }
+        string nonProdDc { get; set; }
+        string multiPodCustomer { get; set; }
+        string notificactionDl { get; set; }
+        string maintanenceDl { get; set; }
+        string customerTyoe { get; set; }
+
+    }
+}
